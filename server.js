2043677a1f4348a6c7105a7c5100a0dc3020ccc9
Kekana Bao <bao.kekana@icloud.com>
2026-08-17 10:09:26 +0200

    Use Render port

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use("/tasks", taskRoutes);
 // MUST BE LAST
 app.use(errorHandler);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
